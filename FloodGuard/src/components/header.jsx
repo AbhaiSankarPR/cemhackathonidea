9a80e6d5185e4e8dbb7381675a6de41f6abdcf42
Abhai Sankar P R <abhaisankarprofficial@gmail.com>
2025-03-15 12:40:26 +0530

    make it responsive step1

--- FloodGuard/src/components/header.jsx
+++ FloodGuard/src/components/header.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './header.css';
+function whichmenu(){
+    
+}
 
 function Header() {
     const navigate = useNavigate();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    
     return (
         <header className="home_header">
             {/* Logo */}
@@ -15,19 +19,23 @@ function Header() {
 
             {/* Menu Button (SVG Icon) - Only for Small Screens */}
             <div className="menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="white">
+                {isMenuOpen ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg> // Close (X) icon
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed">
                     <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
-                </svg>
+                </svg>// Hamburger menu icon
+                )}
             </div>
 
             {/* Navigation Links */}
             <nav className={`nav-links ${isMenuOpen ? "open" : ""}`}>
                 <ul>
-                    <li onClick={() => navigate("/Floodmap")}>Live Flood Map</li>
-                    <li onClick={() => navigate("/MissingPerson")}>Missing Person</li>
-                    <li onClick={() => navigate("/Volunteer")}>Volunteer</li>
+                    <li onClick={() =>{ navigate("/Floodmap"); setIsMenuOpen(!isMenuOpen);}}>Live Flood Map</li>
+                    <li onClick={() => {navigate("/MissingPerson"); setIsMenuOpen(!isMenuOpen);}}>Missing Person</li>
+                    <li onClick={() => {navigate("/Volunteer"); setIsMenuOpen(!isMenuOpen);}}>Volunteer</li>
                     <li>Donate</li>
-                    <li onClick={() => navigate("/About")}>About</li>
+                    <li onClick={() => {navigate("/About"); setIsMenuOpen(!isMenuOpen);}}>About</li>
                 </ul>
             </nav>
         </header>
